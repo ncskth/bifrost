637bbbacc9cc168bcdfdf72b9c537ae39dd61f0c
Gary <chanokin@gmail.com>
2021-09-15 11:52:07 +0100

    requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="bifrost",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    install_requires=["compynator", "torch", "norse"],
+    install_requires=["numpy", "pytest", "compynator", "torch", "norse"],
     scripts=["bin/bifrost"],
 )
